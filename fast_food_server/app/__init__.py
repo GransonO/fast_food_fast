65f1b2c880eb236783dde54b2d78d54969f13588
GransonO <oyombegranson@gmail.com>
2018-09-12 18:18:07 +0300

    [ft#160453770]Create an app instance to be called from the run and test modules

---
+++ fast_food_server/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from config import config_status
+
+
+def create_app(config_state):
+    '''Creates an instance of the app with 
+        necessary configurations passed in '''
+    app = Flask(__name__)
+    
+    if(config_state == 'TESTING'):
+        app.config['TESTING'] = True
+
+    return app
