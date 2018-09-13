d6a019768703f49515e0f62f81518dfe2fee4e3e
GransonO <oyombegranson@gmail.com>
2018-09-13 22:57:54 +0300

    [ft#160388538]Create endpoint to post an order

--- fast_food_server/dataset/data_handler.py
+++ fast_food_server/dataset/data_handler.py
@@ -6,3 +6,11 @@ class savedData():
     def allOrders(self):
         '''Passes the list items back to user'''
         return savedData.Orders
+
+    def addOrders(self,name,amount,quantity):
+        '''Adds an order to the orders list'''
+        id = len(savedData.Orders) + 1
+        passed_order = {'id': id, 'name': name, 'amount' : amount, 'quantity' : quantity}
+        savedData.Orders.append(passed_order)
+
+        return savedData.Orders
--- fast_food_server/run.py
+++ fast_food_server/run.py
@@ -6,6 +6,10 @@ from dataset.data_handler import savedData
 my_app = app.create_app('TESTING')
 api = Api(my_app)
 
+
+properties_order = api.model('order',{'name' : fields.String('Name of what to order'),
+'amount' : fields.Integer('Price of order'),'quantity' : fields.Integer('Quantity of items')})
+
 class Home(Resource):
     def get(self):
         return {'data':'This is home'}
@@ -16,7 +20,19 @@ class GeneralRequests(Resource):
     def get(self):
         '''Retrieves all items and pass them back to user'''
         result = savedData.allOrders(self)
-        return {'data': result}
+        return {'data': result}, 200
+
+    @api.expect(properties_order)
+    def post(self):
+        '''Adds an order to the items list'''
+        my_order = api.payload
+        name = my_order['name']
+        amount = my_order['amount']
+        quantity = my_order['quantity']
+        results = savedData.addOrders(self,name, amount, quantity)
+
+        return {'data':results}, 201
+
    
 
 api.add_resource(Home,'/home')
