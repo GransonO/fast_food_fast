b65177b97c683500e9e2c3afe65dd4cbaebfa445
GransonO <oyombegranson@gmail.com>
2018-09-14 03:19:57 +0300

    [ch#160500176]Ensure linting is done right

--- fast_food_server/app/__init__.py
+++ fast_food_server/app/__init__.py
@@ -1,13 +1,14 @@
+'''creates factory app'''
 from flask import Flask
 from config import config_status
 
 
 def create_app(config_state):
-    '''Creates an instance of the app with 
+    '''Creates an instance of the app with
         necessary configurations passed in '''
     app = Flask(__name__)
-    
-    if(config_state == 'TESTING'):
+
+    if config_state == 'TESTING':
         app.config['TESTING'] = True
         app.config['DEBUG'] = True
 
--- fast_food_server/dataset/data_handler.py
+++ fast_food_server/dataset/data_handler.py
@@ -1,13 +1,13 @@
+'''Class for processing data from requests'''
 class savedData():
-    '''Class for processing data from requests'''
-    
-    Orders = [{'id': 1, 'name':'Default item' , 'amount':10 , 'quantity': 2}]
+
+    Orders = [{'id': 1, 'name':'Default item', 'amount':10, 'quantity': 2}]
 
     def allOrders(self):
         '''Passes the list items back to user'''
         return savedData.Orders
 
-    def addOrders(self,name,amount,quantity):
+    def addOrders(self, name, amount, quantity):
         '''Adds an order to the orders list'''
         id = len(savedData.Orders) + 1
         passed_order = {'id': id, 'name': name, 'amount' : amount, 'quantity' : quantity}
@@ -15,7 +15,7 @@ class savedData():
 
         return savedData.Orders
 
-    def getSpecificOrder(self,id):
+    def getSpecificOrder(self, id):
         '''Retrieves a specific item and sends back to user '''
         num = id - 1
         if len(savedData.Orders) >= num:
@@ -23,9 +23,3 @@ class savedData():
             return item
         else:
             return "Couldn't find what you were looking for" 
-
-        #for order in savedData.Orders:
-        #    if order['id'] == id:
-        #        my_item = order 
-        #    else:
-        #        my_item = "Couldn't find what you were looking for"    
